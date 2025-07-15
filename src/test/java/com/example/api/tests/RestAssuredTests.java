13a2278d5d4bcfeeadbfd888ef69de7defd7262b
Morrowind <gagaegorik@gmail.com>
2025-07-15 11:43:57 +0300

    внес правки

--- src/test/java/com/example/api/tests/RestAssuredTests.java
+++ src/test/java/com/example/api/tests/RestAssuredTests.java
@@ -12,11 +12,9 @@ public class RestAssuredTests {
 
     @BeforeAll
     public static void setup() {
-        // Базовый URL для всех запросов
         RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
     }
 
-    // Тест GET запроса
     @Test
     public void testGetRequest() {
         given()
@@ -28,10 +26,8 @@ public class RestAssuredTests {
                 .body("id", equalTo(1));
     }
 
-    // Тест POST запроса (исправлен для Java 11)
     @Test
     public void testPostRequest() {
-        // Подготовка тела запроса в формате, совместимом с Java 11
         String requestBody = "{\"title\":\"foo\", \"body\":\"bar\", \"userId\":1}";
 
         Response response = given()
